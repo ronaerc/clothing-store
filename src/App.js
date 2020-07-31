0d47a0060f1e09f5189e77a1ed1f51acd7c88a11
Patrick McCreanor <pmccreanor@can-jlc-qejhd4.loycorp.local>
2020-07-31 15:21:16 -0400

    header

--- src/App.js
+++ src/App.js
@@ -5,12 +5,14 @@ import './App.css';
 
 import HomePage from './pages/home/homepage.component';
 import ShopPage from './pages/shop/shoppage.component'
+import Header from './components/header/header.component'
 
 
 
 function App() {
   return (
     <div>
+      <Header />
       <Switch>
         <Route exact path='/' component={HomePage} />
         <Route path='/shop' component={ShopPage} />
